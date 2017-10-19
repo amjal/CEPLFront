143eb7d1ec2318fda32824cae37896a8df5137ea
amir jalili <jalilia370@gmail.com>
2017-10-19 18:09:44 +0330

    added

---
+++ app/view/login/LoginController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by amir on 10/19/17.
+ */
+Ext.define('CEPLFront.view.login.LoginController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.login',
+
+    /**
+     * Called when the view is created
+     */
+    init: function() {
+
+    }
+});
