0cd0d5fc536d2ce8f9cbaf754723d44a5ad15858
sarb <sarb1376@gamil.com>
2017-10-19 13:05:21 +0330

    add logout button again

--- app/view/login/LoginController.js
+++ app/view/login/LoginController.js
@@ -5,65 +5,66 @@ Ext.define('CEPLFront.view.login.LoginController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.login',
 
-    onLoginClick: function ()
-    {
-
-        Ext.Ajax.request({
-            url: 'https://ghiac.ir/bimehtech/api/login',
-
-            method: 'POST',
-
-            params: {
-                ajax_req: Ext.util.JSON.encode({
-                    username: 'test',
-                    password: 'test',
-                    role: "user"
-                })
-            },
-
-            success: function (response, opts)
-            {
-                var obj = Ext.decode(response.responseText);
-
-                // console.dir(obj);
-
-                if (obj.Status === true)
-                {
-                    console.log("logged in!" + response);
-
-                    localStorage.setItem("loginLoggedIn", true);
-
-                    this.getView().destroy();
-                    Ext.create({
-                        xtype: 'app-main'
-                    });
-
-                }
-                else
-                {
-                    console.log("wrong username or password!!");
-                }
-            },
-
-            failure: function (response, opts)
-            {
-                // console.log('server-side failure with status code ' + response.status);
-                console.log("error!");
-            }
-        });
-    }
-
     // onLoginClick: function ()
     // {
-    //     // Set the localStorage value to true
-    //     localStorage.setItem("loggedIn", true);
-    //
-    //     // Remove Login Window
-    //     this.getView().destroy();
-    //
-    //     // Add the main view to the viewport
-    //     Ext.create({
-    //         xtype: 'app-main'
-    //     });
+
+        //     Ext.Ajax.request({
+        //         url: 'https://ghiac.ir/bimehtech/api/login',
+        //
+        //         method: 'POST',
+        //
+        //         params: {
+        //             ajax_req: Ext.util.JSON.encode({
+        //                 username: 'test',
+        //                 password: 'test',
+        //                 role: "user"
+        //             })
+        //         },
+        //
+        //         success: function (response, opts)
+        //         {
+        //             var obj = Ext.decode(response.responseText);
+        //
+        //             // console.dir(obj);
+        //
+        //             if (obj.Status === true)
+        //             {
+        //                 console.log("logged in!" + response);
+        //
+        //                 localStorage.setItem("loginLoggedIn", true);
+        //
+        //                 this.getView().destroy();
+        //                 Ext.create({
+        //                     xtype: 'app-main'
+        //                 });
+        //
+        //             }
+        //             else
+        //             {
+        //                 console.log("wrong username or password!!");
+        //             }
+        //         },
+        //
+        //         failure: function (response, opts)
+        //         {
+        //             // console.log('server-side failure with status code ' + response.status);
+        //             console.log("error!");
+        //         }
+        //     });
+        // }
+
+        onLoginClick: function ()
+        {
+            // Set the localStorage value to true
+            localStorage.setItem("loggedIn", true);
+
+            // Remove Login Window
+            this.getView().destroy();
+
+            // Add the main view to the viewport
+            Ext.create({
+                xtype: 'app-main'
+            });
+        }
     // }
 });
