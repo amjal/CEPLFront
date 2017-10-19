a10f346f6b6f7842f03b4dc9a639b551714216a8
sarb <sarb1376@gamil.com>
2017-10-19 18:11:59 +0330

    test

--- app/Application.js
+++ app/Application.js
@@ -14,15 +14,8 @@ Ext.define('CEPLFront.Application', {
 
     launch: function () {
 
-        // It's important to note that this type of application could use
-        // any type of storage, i.e., Cookies, LocalStorage, etc.
-
-        // Check to see the current value of the localStorage key
         var loggedIn = localStorage.getItem("loggedIn");
         alert(loggedIn);
-        // This ternary operator determines the value of the TutorialLoggedIn key.
-        // If TutorialLoggedIn isn't true, we display the login window,
-        // otherwise, we display the main view
         Ext.create({
             xtype: loggedIn ? 'app-main' : 'login'
         });
