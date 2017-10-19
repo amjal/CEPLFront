1e73e1925fc147f5a890a2943a0cfc6e0abd96e5
amir jalili <jalilia370@gmail.com>
2017-10-19 14:33:30 +0330

    correct login controller

--- app/view/login/LoginController.js
+++ app/view/login/LoginController.js
@@ -5,66 +5,62 @@ Ext.define('CEPLFront.view.login.LoginController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.login',
 
+    onLoginClick: function ()
+    {
+        Ext.Ajax.request({
+            url: 'HTTP://192.168.110.103:8080/bimehtech/api/login',
+
+            method: 'POST',
+
+            params: {
+                ajax_req: Ext.util.JSON.encode({
+                    username: 'test',
+                    password: 'test',
+                    role: "user"
+                })
+            },
+
+            success: function (response, opts)
+            {
+                var responseTxt = JSON.parse(response.responseText);
+
+                if (responseTxt.logged === true)
+                {
+                    console.log("logged in!" + response);
+
+                    localStorage.setItem("loginLoggedIn", true);
+
+                    Ext.getCmp('loginPage').destroy();
+                    Ext.create({
+                        xtype: 'app-main'
+                    });
+
+                }
+                else
+                {
+                    console.log("wrong username or password!!");
+                }
+            },
+
+            failure: function (response, opts)
+            {
+                // console.log('server-side failure with status code ' + response.status);
+                console.log("error!");
+            }
+        });
+    }
+
     // onLoginClick: function ()
     // {
-
-        //     Ext.Ajax.request({
-        //         url: 'https://ghiac.ir/bimehtech/api/login',
-        //
-        //         method: 'POST',
-        //
-        //         params: {
-        //             ajax_req: Ext.util.JSON.encode({
-        //                 username: 'test',
-        //                 password: 'test',
-        //                 role: "user"
-        //             })
-        //         },
-        //
-        //         success: function (response, opts)
-        //         {
-        //             var obj = Ext.decode(response.responseText);
-        //
-        //             // console.dir(obj);
-        //
-        //             if (obj.Status === true)
-        //             {
-        //                 console.log("logged in!" + response);
-        //
-        //                 localStorage.setItem("loginLoggedIn", true);
-        //
-        //                 this.getView().destroy();
-        //                 Ext.create({
-        //                     xtype: 'app-main'
-        //                 });
-        //
-        //             }
-        //             else
-        //             {
-        //                 console.log("wrong username or password!!");
-        //             }
-        //         },
-        //
-        //         failure: function (response, opts)
-        //         {
-        //             // console.log('server-side failure with status code ' + response.status);
-        //             console.log("error!");
-        //         }
-        //     });
-        // }
-
-        onLoginClick: function ()
-        {
-            // Set the localStorage value to true
-            localStorage.setItem("loggedIn", true);
-
-            // Remove Login Window
-            this.getView().destroy();
-
-            // Add the main view to the viewport
-            Ext.create({
-                xtype: 'app-main'
-            });
-        }
+    //     // Set the localStorage value to true
+    //     localStorage.setItem("loggedIn", true);
+    //
+    //     // Remove Login Window
+    //     this.getView().destroy();
+    //
+    //     // Add the main view to the viewport
+    //     Ext.create({
+    //         xtype: 'app-main'
+    //     });
     // }
 });
